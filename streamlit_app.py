18daa902d7c211098697a81edf9f42e87faccb42
christineanngonzales <120782002+christineanngonzales@users.noreply.github.com>
2022-12-20 15:27:31 +0800

    Update streamlit_app.py
    add normalize table

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
-# write your own comment -what does the next line do? 
+# write your own comment -what does the next line do? Normalize the table 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
+# write your own comment - what does this do? display the normalize table
 streamlit.dataframe(fruityvice_normalized)
